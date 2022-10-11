357c5532a6305d3003186e053dd66b24a2ad618f
Thor Anderson <ta.anderson@gmail.com>
2022-10-11 12:33:52 -0600

    Got a for loop modifying the DOM!

--- CodeJournal.js
+++ CodeJournal.js
@@ -26,3 +26,42 @@ let myNum2 = 75.25
 "1" // is not a number!  It is a string
 "1" == 1 // evaluates to true because of type coercion and loose equality checking
 "1" === 1 // evaluates to false because it forces string equality checking
+
+// Boolean
+
+let myBool = false
+
+// need to look further into "truthy" and "falsy" values
+
+// Arrays - hold sets of items of any data type
+
+let mySimpleArray = [] // this is an empty array - notice the square brackets
+
+// ordering     0    1      2        3      4
+let myArray = [42, "Bob", myBool, ANSWER, true] // arrays contain any mix of data types
+
+let firstItem = myArray[0]  // in this case firstItem === 42
+
+const lastItem = myArray[myArray.length - 1]
+
+// Objects
+
+let minObject = {} // valid, most minimal object possible
+
+let myCar = {
+    make: 'Chevrolet',
+    color: 'Green',
+    year: 1964,
+    vin: '20349830948LKJDFLKJ'
+}
+
+myCar.numDoors = 4 // a new property can be simply added to an existing object using dot notation!
+
+const anotherObject = { // objects can contain just about anything
+    wordz: ['foo', 'bar', 'baz'],
+    car: {
+        make: 'McLaren',
+        model: '720s'
+    },
+    awesomeness: true
+}
--- films/starwars-films.js
+++ films/starwars-films.js
@@ -1,4 +1,12 @@
+import { films } from '../data/films.js'
 
 
+let filmTitles = document.querySelector('#filmTitles')
 
-console.log("Hello World!")
+//filmTitles.textContent = 'some random text I am showing here'
+
+for (let i = 0; i < films.length; i++) {
+    const newParagraph = document.createElement("p")
+    newParagraph.textContent = films[i].title
+    filmTitles.appendChild(newParagraph)
+ }
