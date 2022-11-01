5d23a5ad297041966601778d05f4c32c81d0fbbc
Thor Anderson <ta.anderson@gmail.com>
2022-11-01 12:35:19 -0600

    Basically working characters page

--- characters/sw-chars.js
+++ characters/sw-chars.js
@@ -1,23 +1,54 @@
 import { people } from '../data/people.js'
 
-//console.log(people)
-
+const header = document.querySelector('header')
 const main = document.querySelector('main')
 
-people.forEach(person => {
-    let figure = document.createElement('figure')
-    let figImage = document.createElement('img')
-    let figCaption = document.createElement('figcaption')
-
-    let charNum = getLastNumber(person.url)
-    
-    figImage.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
-    figCaption.textContent = person.name
-
-    figure.appendChild(figImage)
-    figure.appendChild(figCaption)
-    main.appendChild(figure)
+const allCharsButton = document.createElement('button')
+allCharsButton.textContent = 'All Characters'
+allCharsButton.addEventListener('click', function () {
+    populateDOM(people)
 })
+
+const maleCharacters = people.filter(person => person.gender === 'male')
+const femaleCharacters = people.filter(person => person.gender === 'female')
+const otherCharacters = people.filter(person => person.gender !== 'female' && person.gender !== 'male')
+
+const maleCharsButton = document.createElement('button')
+maleCharsButton.textContent = 'Male Characters'
+maleCharsButton.addEventListener('click', () => populateDOM(maleCharacters))
+
+const femaleCharsButton = document.createElement('button')
+femaleCharsButton.textContent = 'Female Characters'
+femaleCharsButton.addEventListener('click', () => populateDOM(femaleCharacters))
+
+const otherCharsButton = document.createElement('button')
+otherCharsButton.textContent = 'Other Characters'
+otherCharsButton.addEventListener('click', () => populateDOM(otherCharacters))
+
+header.appendChild(allCharsButton)
+header.appendChild(maleCharsButton)
+header.appendChild(femaleCharsButton)
+header.appendChild(otherCharsButton)
+
+function populateDOM(arrayOfCharacters) {
+    while (main.firstChild) { // light saber activates... until all children removed
+        main.removeChild(main.firstChild);
+      }
+    arrayOfCharacters.forEach(person => {
+        let figure = document.createElement('figure')
+        let figImage = document.createElement('img')
+        let figCaption = document.createElement('figcaption')
+
+        let charNum = getLastNumber(person.url)
+    
+        figImage.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+        figCaption.textContent = person.name
+
+        figure.appendChild(figImage)
+        figure.appendChild(figCaption)
+        main.appendChild(figure)
+    })
+}
 
 // 'https://swapi.co/api/people/8/'
 
@@ -29,4 +60,6 @@ function getLastNumber(url) {
         start++
     }
     return url.slice(start, end)
-  }
+}
+  
+populateDOM(people)
